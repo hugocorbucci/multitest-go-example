881e736447f4c35448d477fb007c776dab9ed935
Hugo Corbucci <hugo.corbucci@gmail.com>
2019-10-30 12:12:27 -0400

    Add test to verify mux mapping

--- internal/server/http_test.go
+++ internal/server/http_test.go
@@ -1,6 +1,9 @@
 package server_test
 
 import (
+	"context"
+	"io/ioutil"
+	"net"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -21,4 +24,40 @@ func TestHomeReturnsHelloWorldLocal(t *testing.T) {
 	require.Equal(t, http.StatusOK, w.Code, "expected status code to match for req %+v", httpReq)
 	body := string(w.Body.Bytes())
 	assert.Equal(t, "Hello, world", body, "expected body to match")
+}
+
+func TestHomeReturnsHelloWorld(t *testing.T) {
+	s := server.NewHTTPServer()
+
+	listener, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("could not listen for HTTP requests: %+v", err)
+	}
+	baseURL := "http://" + listener.Addr().String()
+	srvr := http.Server{Addr: baseURL, Handler: s}
+
+	go srvr.Serve(listener)
+	defer func() {
+		if err := srvr.Shutdown(context.Background()); err != nil {
+			t.Logf("could not shutdown http server: %+v", err)
+		}
+	}()
+
+	httpReq, err := http.NewRequest(http.MethodGet, baseURL+"/", nil)
+	require.NoError(t, err, "could not create GET / request")
+
+	resp, err := http.DefaultClient.Do(httpReq)
+	require.NoError(t, err, "error making request %+v", httpReq)
+	require.Equal(t, http.StatusOK, resp.StatusCode, "expected status code to match for req %+v", httpReq)
+	body, err := readBodyFrom(resp)
+	require.NoError(t, err, "unexpected error reading response body")
+	assert.Equal(t, "Hello, world", body, "expected body to match")
+}
+
+func readBodyFrom(resp *http.Response) (string, error) {
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(bodyBytes), nil
 }
